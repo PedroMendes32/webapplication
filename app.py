fd0868b3453ecccb75131b47ccee063794f20237
PedroMendes32 <pedrocmendescoc2@gmail.com>
2022-06-08 13:51:20 +0000

    funcao edit

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect 
 from uuid import uuid4
 
 app = Flask(__name__)
 
 agenda = [ { 'id':uuid4(),'Tarefa':'Caminhar com o cachorro','Status':'Realizada','Horário':"18:40",'Tipo':'Lazer'}]
 
-           
 @app.route('/inicio')
 def inicio():
     return render_template('index.html',agenda = agenda )
@@ -20,12 +19,25 @@ def save():
     horario = request.form['Horário']
     tipo = request.form['Tipo']
     agenda.append({'id':uuid4(),'Tarefa':tarefa,'Status':'Não realizada','Horário':horario,'Tipo':tipo})
-    return render_template('index.html', agenda = agenda )
+    return redirect('\inicio')
 
 @app.route('/edit/<id>')
 def edit(id):
-    return render_template('update.html')
+    for tarefa in agenda:
+        if id == str(tarefa['id']):
+            return render_template('update.html',tarefa = tarefa)
 
-   
+@app.route('/edit/tarefa/<id>', methods=['POST'])
+def salvar_edicao(id):
+    for tarefa in agenda:
+        if id == str(tarefa['id']):
+            i = agenda.index(tarefa)
+            id_modificado = tarefa['id']
+    n_tarefa = request.form['Tarefa']
+    n_status = request.form['Status']
+    n_horario = request.form['Horário']
+    n_tipo = request.form['Tipo']
+    agenda[i] = {'id':id_modificado,'Tarefa':n_tarefa,'Status':n_status,'Horário':n_horario,'Tipo':n_tipo}
+    return redirect('/inicio')
 
 app.run(debug=True)
